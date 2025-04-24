833255fa2016debbef1a60df1f16e3225c440ff7
七喜 <qijoy7@gmail.com>
2025-04-24 10:19:17 +0800

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -8,11 +8,9 @@ import aiohttp
 
 from github_stats import Stats
 
-
 ################################################################################
 # Helper Functions
 ################################################################################
-
 
 def generate_output_folder() -> None:
     """
@@ -21,11 +19,9 @@ def generate_output_folder() -> None:
     if not os.path.isdir("generated"):
         os.mkdir("generated")
 
-
 ################################################################################
 # Individual Image Generation Functions
 ################################################################################
-
 
 async def generate_overview(s: Stats) -> None:
     """
@@ -48,7 +44,6 @@ async def generate_overview(s: Stats) -> None:
     with open("generated/overview.svg", "w") as f:
         f.write(output)
 
-
 async def generate_languages(s: Stats) -> None:
     """
     Generate an SVG badge with summary languages used
@@ -64,8 +59,7 @@ async def generate_languages(s: Stats) -> None:
     )
     delay_between = 150
     for i, (lang, data) in enumerate(sorted_languages):
-        color = data.get("color")
-        color = color if color is not None else "#000000"
+        color = data.get("color") or "#000000"
         progress += (
             f'<span style="background-color: {color};'
             f'width: {data.get("prop", 0):0.3f}%;" '
@@ -79,7 +73,6 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">{lang}</span>
 <span class="percent">{data.get("prop", 0):0.2f}%</span>
 </li>
-
 """
 
     output = re.sub(r"{{ progress }}", progress, output)
@@ -89,11 +82,9 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
     with open("generated/languages.svg", "w") as f:
         f.write(output)
 
-
 ################################################################################
 # Main Function
 ################################################################################
-
 
 async def main() -> None:
     """
@@ -101,25 +92,19 @@ async def main() -> None:
     """
     access_token = os.getenv("ACCESS_TOKEN")
     if not access_token:
-        # access_token = os.getenv("GITHUB_TOKEN")
         raise Exception("A personal access token is required to proceed!")
-    user = os.getenv("GITHUB_ACTOR")
-    if user is None:
-        raise RuntimeError("Environment variable GITHUB_ACTOR must be set.")
+
+    user = "qijoy"
+
     exclude_repos = os.getenv("EXCLUDED")
-    excluded_repos = (
-        {x.strip() for x in exclude_repos.split(",")} if exclude_repos else None
-    )
+    excluded_repos = {x.strip() for x in exclude_repos.split(",")} if exclude_repos else None
     exclude_langs = os.getenv("EXCLUDED_LANGS")
-    excluded_langs = (
-        {x.strip() for x in exclude_langs.split(",")} if exclude_langs else None
-    )
-    # Convert a truthy value to a Boolean
+    excluded_langs = {x.strip() for x in exclude_langs.split(",")} if exclude_langs else None
     raw_ignore_forked_repos = os.getenv("EXCLUDE_FORKED_REPOS")
     ignore_forked_repos = (
-        not not raw_ignore_forked_repos
-        and raw_ignore_forked_repos.strip().lower() != "false"
+        not not raw_ignore_forked_repos and raw_ignore_forked_repos.strip().lower() != "false"
     )
+
     async with aiohttp.ClientSession() as session:
         s = Stats(
             user,
@@ -130,7 +115,6 @@ async def main() -> None:
             ignore_forked_repos=ignore_forked_repos,
         )
         await asyncio.gather(generate_languages(s), generate_overview(s))
-
 
 if __name__ == "__main__":
     asyncio.run(main())
